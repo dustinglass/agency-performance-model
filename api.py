ba9bb33a7073a8fdf30a7f67741b52d8ac474bbf
dustinglass <dustin.glass@gmail.com>
2019-05-16 18:50:12 -0300

    update details and summary get methods

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
 from io import StringIO, BytesIO
 
-from flask import Flask, request, send_file, jsonify
+from flask import Flask, request, send_file, jsonify, abort
 from flask_restful import Resource, Api
 from pandas import read_sql
 from sqlalchemy import create_engine
+
 
 PARAM_TABLE_MAP = {
     'AGENCY_ID': 'insurance',
@@ -26,12 +27,13 @@ PARAM_TABLE_MAP = {
 JOIN_TEMPLATE = ' INNER JOIN {0} ON {0}.id = insurance.{1}_ID'
 WHERE_TEMPLATE = "{}.{} = '{}'"
 
+
 app = Flask(__name__)
 api = Api(app)
 
 
 class InvalidParameter(Exception):
-    """Return descriptive parameter exception."""
+    """Return message about invalid parameter."""
 
     status_code = 422
     message_template = 'Invalid parameter `{}`.'
@@ -47,12 +49,40 @@ class InvalidParameter(Exception):
         return {'message': self.message}
 
 
-@app.errorhandler(InvalidParameter)
-def handle_invalid_parameter(error):
-    """Handle InvalidParameter exception in Flask app."""
+class MissingParameter(Exception):
+    """Return message about missing parameter."""
+
+    status_code = 422
+    message_template = 'Missing required parameter `{}`.'
+
+    def __init__(self, param=None, custom_message_template=None):
+        Exception.__init__(self)
+        if custom_message_template:
+            self.message = custom_message_template.format(param)
+        else:
+            self.message = self.message_template.format(param)
+
+    def to_dict(self):
+        return {'message': self.message}
+
+
+def handle_parameter_exception(error):
+    """Handle generic parameter exception in Flask app."""
     response = jsonify(error.to_dict())
     response.status_code = error.status_code
     return response
+
+
+@app.errorhandler(InvalidParameter)
+def handle_invalid_parameter(error):
+    """Handle InvalidParameter exception in Flask app."""
+    return handle_parameter_exception(error)
+
+
+@app.errorhandler(MissingParameter)
+def handle_missing_parameter(error):
+    """Handle InvalidParameter exception in Flask app."""
+    return handle_parameter_exception(error)
 
 
 def _select_df(sql, dbapi='sqlite:///insurance.db'):
@@ -83,15 +113,25 @@ def _build_out_sql(sql, request_args):
     return sql
 
 
-class Detail(Resource):
+def _check_params(required_params):
+    for param in required_params:
+        if param not in request.args.keys():
+            raise MissingParameter(param)
+    for param in request.args.keys():
+        if param not in PARAM_TABLE_MAP \
+        and param not in required_params:
+            raise InvalidParameter(param)
+
+
+class Details(Resource):
     """Return detailed information using different parameters."""
+
+    required_params = ['AGENCY_ID', 'PROD_LINE']
     
     def get(self):
-        for param in request.args.keys():
-            if param not in PARAM_TABLE_MAP:
-                raise InvalidParameter(param)
+        _check_params(self.required_params)
 
-        sql = 'SELECT insurance.* FROM insurance'   
+        sql = 'SELECT insurance.* FROM insurance'
         sql = _build_out_sql(sql, request.args)
         return _sql_response(sql, request.args)
 
@@ -99,42 +139,46 @@ class Detail(Resource):
 class Summary(Resource):
     """Return summarized information using different parameters."""
 
+    required_params = ['AGG']
+
     def get(self):
-        for param in request.args.keys():
-            if param not in PARAM_TABLE_MAP:
-                raise InvalidParameter(param)
+        _check_params(self.required_params)
 
         sql = '''SELECT
-            COUNT(*) AS RESULTS_COUNT, 
-            SUM(RETENTION_POLY_QTY) AS RETENTION_POLY_QTY_SUM,
-            SUM(POLY_INFORCE_QTY) AS POLY_INFORCE_QTY_SUM,
-            SUM(PREV_POLY_INFORCE_QTY) AS PREV_POLY_INFORCE_QTY_SUM,
-            SUM(NB_WRTN_PREM_AMT) AS NB_WRTN_PREM_AMT_SUM,
-            SUM(WRTN_PREM_AMT) AS WRTN_PREM_AMT_SUM,
-            SUM(PREV_WRTN_PREM_AMT) AS PREV_WRTN_PREM_AMT_SUM,
-            SUM(PRD_ERND_PREM_AMT) AS PRD_ERND_PREM_AMT_SUM,
-            SUM(PRD_INCRD_LOSSES_AMT) AS PRD_INCRD_LOSSES_AMT_SUM,
-            AVG(RETENTION_RATIO) AS RETENTION_RATIO_AVG,
-            AVG(LOSS_RATIO) AS LOSS_RATIO_AVG,
-            AVG(LOSS_RATIO_3YR) AS LOSS_RATIO_3YR_AVG,
-            AVG(GROWTH_RATE_3YR) AS GROWTH_RATE_3YR_AVG,
-            SUM(CL_BOUND_CT_MDS) AS CL_BOUND_CT_MDS_SUM,
-            SUM(CL_QUO_CT_MDS) AS CL_QUO_CT_MDS_SUM,
-            SUM(CL_BOUND_CT_SBZ) AS CL_BOUND_CT_SBZ_SUM,
-            SUM(CL_QUO_CT_SBZ) AS CL_QUO_CT_SBZ_SUM,
-            SUM(CL_QUO_CT_EQT) AS CL_QUO_CT_EQT_SUM,
-            SUM(PL_BOUND_CT_ELINKS) AS PL_BOUND_CT_ELINKS_SUM,
-            SUM(PL_QUO_CT_ELINKS) AS PL_QUO_CT_ELINKS_SUM,
-            SUM(PL_BOUND_CT_PLRANK) AS PL_BOUND_CT_PLRANK_SUM,
-            SUM(PL_QUO_CT_PLRANK) AS PL_QUO_CT_PLRANK_SUM,
-            SUM(PL_BOUND_CT_EQTTE) AS PL_BOUND_CT_EQTTE_SUM,
-            SUM(PL_QUO_CT_EQTTE) AS PL_QUO_CT_EQTTE_SUM,
-            SUM(PL_BOUND_CT_APPLIED) AS PL_BOUND_CT_APPLIED_SUM,
-            SUM(PL_QUO_CT_APPLIED) AS PL_QUO_CT_APPLIED_SUM,
-            SUM(PL_BOUND_CT_TRANSACTNOW) AS PL_BOUND_CT_TRANSACTNOW_SUM,
-            SUM(PL_QUO_CT_TRANSACTNOW) AS PL_QUO_CT_TRANSACTNOW_SUM
-        FROM INSURANCE''' 
-        sql = _build_out_sql(sql, request.args)
+            COUNT(*) AS RESULTS_COUNT,
+            '{0}' AS AGG,
+            {0}(RETENTION_POLY_QTY) AS RETENTION_POLY_QTY,
+            {0}(POLY_INFORCE_QTY) AS POLY_INFORCE_QTY,
+            {0}(PREV_POLY_INFORCE_QTY) AS PREV_POLY_INFORCE_QTY,
+            {0}(NB_WRTN_PREM_AMT) AS NB_WRTN_PREM_AMT,
+            {0}(WRTN_PREM_AMT) AS WRTN_PREM_AMT,
+            {0}(PREV_WRTN_PREM_AMT) AS PREV_WRTN_PREM_AMT,
+            {0}(PRD_ERND_PREM_AMT) AS PRD_ERND_PREM_AMT,
+            {0}(PRD_INCRD_LOSSES_AMT) AS PRD_INCRD_LOSSES_AMT,
+            {0}(RETENTION_RATIO) AS RETENTION_RATIO,
+            {0}(LOSS_RATIO) AS LOSS_RATIO,
+            {0}(LOSS_RATIO_3YR) AS LOSS_RATIO_3YR,
+            {0}(GROWTH_RATE_3YR) AS GROWTH_RATE_3YR,
+            {0}(CL_BOUND_CT_MDS) AS CL_BOUND_CT_MDS,
+            {0}(CL_QUO_CT_MDS) AS CL_QUO_CT_MDS,
+            {0}(CL_BOUND_CT_SBZ) AS CL_BOUND_CT_SBZ,
+            {0}(CL_QUO_CT_SBZ) AS CL_QUO_CT_SBZ,
+            {0}(CL_QUO_CT_EQT) AS CL_QUO_CT_EQT,
+            {0}(PL_BOUND_CT_ELINKS) AS PL_BOUND_CT_ELINKS,
+            {0}(PL_QUO_CT_ELINKS) AS PL_QUO_CT_ELINKS,
+            {0}(PL_BOUND_CT_PLRANK) AS PL_BOUND_CT_PLRANK,
+            {0}(PL_QUO_CT_PLRANK) AS PL_QUO_CT_PLRANK,
+            {0}(PL_BOUND_CT_EQTTE) AS PL_BOUND_CT_EQTTE,
+            {0}(PL_QUO_CT_EQTTE) AS PL_QUO_CT_EQTTE,
+            {0}(PL_BOUND_CT_APPLIED) AS PL_BOUND_CT_APPLIED,
+            {0}(PL_QUO_CT_APPLIED) AS PL_QUO_CT_APPLIED,
+            {0}(PL_BOUND_CT_TRANSACTNOW) AS PL_BOUND_CT_TRANSACTNOW,
+            {0}(PL_QUO_CT_TRANSACTNOW) AS PL_QUO_CT_TRANSACTNOW
+        FROM INSURANCE'''.format(request.args['AGG'])
+        args = dict(request.args)
+        for i in self.required_params:
+            del args[i]
+        sql = _build_out_sql(sql, args)
         return _sql_response(sql, request.args)
 
 
@@ -149,10 +193,10 @@ class Report(Resource):
         sql = '''SELECT
             i.AGENCY_ID AS agency_id,
             p.PROD_LINE AS product_line,
-            SUM(i.NB_WRTN_PREM_AMT) AS nb_wrtn_prem_amt_sum,
-            SUM(i.WRTN_PREM_AMT) AS wrtn_prem_amt_sum,
-            SUM(i.PREV_WRTN_PREM_AMT) AS prev_wrtn_prem_amt_sum,
-            SUM(i.PRD_ERND_PREM_AMT) AS prd_ernd_prem_amt_sum
+            SUM(i.NB_WRTN_PREM_AMT) AS nb_wrtn_prem_amt,
+            SUM(i.WRTN_PREM_AMT) AS wrtn_prem_amt,
+            SUM(i.PREV_WRTN_PREM_AMT) AS prev_wrtn_prem_amt,
+            SUM(i.PRD_ERND_PREM_AMT) AS prd_ernd_prem_amt
         FROM insurance i
         INNER JOIN product p ON p.id = i.PRODUCT_ID
         '''
@@ -180,7 +224,7 @@ class Report(Resource):
                          attachment_filename='report.csv')
 
 
-api.add_resource(Detail, '/detail')
+api.add_resource(Details, '/details')
 api.add_resource(Summary, '/summary')
 api.add_resource(Report, '/report')
 
